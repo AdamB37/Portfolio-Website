3cf0570fd4dbec1e786e0c429858c26303a22168
AdamB37 <adam.beshir@gmail.com>
2017-07-06 09:35:32 -0700

    wip

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var path = require('path')
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
